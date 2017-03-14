f7f0e7621f577c5085f97fb0674e30a28658a443
Peter Csajtai <peter.csajtai@outlook.com>
2017-03-14 04:13:25 +0100

    Updated stashbox, added test project

--- src/stashbox.web.webapi/HttpConfigurationExtensions.cs
+++ src/stashbox.web.webapi/HttpConfigurationExtensions.cs
@@ -5,6 +5,7 @@ using System.Web.Http.Validation.Providers;
 using Stashbox;
 using Stashbox.Entity;
 using Stashbox.Infrastructure;
+using Stashbox.Lifetime;
 using Stashbox.Web.WebApi;
 
 namespace System.Web.Http
@@ -42,8 +43,22 @@ namespace System.Web.Http
             config.Services.Clear(typeof(ModelValidatorProvider));
 
             config.DependencyResolver = new StashboxDependencyResolver(StashboxConfig.Container);
-
+            config.RegisterWebApiControllers();
             configureAction(StashboxConfig.Container);
+        }
+
+        /// <summary>
+        /// Registers the web api controllers into the <see cref="IStashboxContainer"/>.
+        /// </summary>
+        /// <param name="config">The http configuration.</param>
+        public static void RegisterWebApiControllers(this HttpConfiguration config)
+        {
+            var assembliesResolver = config.Services.GetAssembliesResolver();
+            var typeResolver = config.Services.GetHttpControllerTypeResolver();
+            var controllers = typeResolver.GetControllerTypes(assembliesResolver);
+
+            foreach (var controller in controllers)
+                StashboxConfig.Container.PrepareType(controller).WithLifetime(new ScopedLifetime()).Register();
         }
     }
 }
